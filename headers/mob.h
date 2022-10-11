00cbfafb850fda1c3ed7faafe49e7a627ea3f609
Suna24 <btarot@et.esiea.fr>
2022-10-11 15:46:46 +0200

    list of mobs

--- headers/mob.h
+++ headers/mob.h
@@ -2,6 +2,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* All stregth type */
+#define VERY_SMALL 500
+#define SMALL 501
+#define MEDIUM 502
+#define LARGE 503
+#define VERY_LARGE 504
+#define GIGANTIC 505
+#define TOTAL_NUMBER_OF_MOBS 50
+
 /* Struct of the monster */
 typedef struct Mob
 {
@@ -11,7 +20,9 @@ typedef struct Mob
     int coordX;
     int coordY;
     int isDead;
+    int strength;
 } Mob;
 
 void freeMobs(Mob **mobs, int nbMobs);
-char *generateRandomMob();
+Mob **initMobs();
+Mob **generateRandomMobs(Mob **mobs, int numberOfMobs);
